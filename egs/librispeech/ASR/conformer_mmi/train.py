da55e3376af1040e8c9b07ceef900188f008a7de
Umberto Cappellazzo <umbertocappellazzo@MacBook-Pro-di-Umberto.local>
2023-07-07 23:43:03 +0200

    Fixed path to checkpoint

--- egs/librispeech/ASR/conformer_mmi/train.py
+++ egs/librispeech/ASR/conformer_mmi/train.py
@@ -258,7 +258,7 @@ def load_checkpoint_if_available(
     """
     if params.start_epoch <= 0:
         return
-    filename = Path('conformer_ctc/exp') / f"epoch-{params.start_epoch-1}.pt"
+    filename = Path('conformer_ctc/exp_100libri') / f"epoch-{params.start_epoch-1}.pt"
     #filename = '/cappellazzo/icefall_forked/icefall/egs/librispeech/ASR/conformer_ctc/exp'
     #filename = params.exp_dir / f"epoch-{params.start_epoch-1}.pt"
     saved_params = load_checkpoint(
